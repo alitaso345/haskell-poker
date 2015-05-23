d836d408a3ac34f164cb6254bf9f1eb87ee75a23
alice345 <alice.maru345@gmail.com>
2015-05-24 02:16:18 +0900

    toHand関数に書き換え

--- Hands.hs
+++ Hands.hs
@@ -1,7 +1,7 @@
 newtype Hand = Hand { fromHand :: [Card] } deriving (Show, Eq, Ord)
 
-dicision :: [Card] -> Maybe [Card]
-dicision l =
+toHand :: [Card] -> Maybe Hand
+toHand l =
   if length l == 5
     then Just $ sort l
     else Nothing
