0a58dda58adc285f06a597ce71f7beff14b5effd
alice345 <alice.maru345@gmail.com>
2015-06-27 16:53:59 +0900

    プレイヤー操作の部分を実装

--- Main.hs
+++ Main.hs
@@ -8,9 +8,59 @@ import Safe
 
 main :: IO ()
 main = do
-  hand <- randomHand
-  let res = judgePoker hand
-  print $ show hand ++ " -> " ++ show res
+  putStrLn "------------------"
+  putStrLn "---simple poker---"
+  putStrLn "------------------"
+  deck <- shuffleM allCards
+  case getHand deck of
+    Nothing -> error "予期せぬエラー"
+    Just (hand, deck) -> playPoker hand deck
+  ynQuestion "-- もっかいやる？" main (putStrLn "--ノシノシ")
+
+playPoker :: Hand -> Deck -> IO ()
+playPoker hand deck = do
+  discards <- inputDisuse hand
+  case drawHand deck discards hand of
+    Nothing -> error "予期せぬエラー"
+    Just (nhand, _) -> do
+      printHand [] nhand
+      printResult $ pokerHand nhand
+
+inputDisuse :: Hand -> IO DiscardList
+inputDisuse hand = do
+  printHand [] hand
+  putStrLn "-- 捨てるカードを選んでね"
+  gotDisuse <- getDiscardList hand
+  case gotDisuse of
+    Nothing -> do
+      putStrLn "-- 1-5の数値を並べて入力してね"
+      inputDisuse hand
+    Just disuses -> do
+      printHand disuses hand
+      ynQuestion "-- これでいい？" (return disuses) (inputDisuse hand)
+
+printResult ::(PokerHand, Card) -> IO ()
+printResult (ph, card) = putStrLn $ concat
+  ["***** あなたの手札は ", show ph, " で，最強カードは ", show card, " でした*****"]
+
+printHand :: DiscardList -> Hand -> IO ()
+printHand dis hand = putStrLn $ "-- 手札: " ++ showChangeHand dis hand
+
+ynQuestion :: String -> IO a -> IO a -> IO a
+ynQuestion s yes no = do
+  putStrLn $ s ++ "(y/n)"
+  input <- getLine
+  case input of
+    "y" -> yes
+    "n" -> no
+    _ -> do
+      putStrLn "-- `y`か`n`で入力してね"
+      ynQuestion s yes no
+
+showChangeHand :: DiscardList -> Hand -> String
+showChangeHand dis h = let
+  judge x = if elem x dis then " " ++ show x ++ " " else "[" ++ show x ++ "]"
+  in concat $ map judge (fromHand h)
 
 randomHand :: IO (Maybe Hand)
 randomHand = do
